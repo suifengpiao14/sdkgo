1ba0c0bc3d557b88e747bd3d067422a08bd75231
pz <2912150017@qq.com>
2023-12-02 11:28:12 +0800

    add string

--- config.go
+++ config.go
@@ -1,7 +1,15 @@
 package sdkgolib
 
+import "encoding/json"
+
 type Config struct {
 	Host          string `mapstructure:"url" json:"url" validate:"required"`
 	CallerID      string `mapstructure:"callerId" json:"callerId"  validate:"required"`
 	CallerAppName string `mapstructure:"callerAppName" json:"callerAppName"  validate:"required"`
+}
+
+func (c Config) String() string {
+	b, _ := json.Marshal(c)
+	s := string(b)
+	return s
 }
